9a6c4477262dd48dc36fa02b3bc364d6050d7251
Katranas <148208752+Katranas@users.noreply.github.com>
2023-11-03 12:55:17 +0200

    Fixed Director.h

--- Builder/Director.h
+++ Builder/Director.h
@@ -2,6 +2,7 @@
 #define DIRECTOR_H
 
 #include "Builder.h"
+#include "MealBuilder.h"
 #include "Pizza.h"
 #include <string>
 /**
@@ -9,9 +10,14 @@
  */
 class Director {
     public:
-
+        /**
+         * Constructor for Director class.
+         * @param builder The concrete Builder that will build obect (e.g., "MealBuilder").
+         */
         Director(Builder* builder);
+        ~Director();
         void make(Pizza* pizza);
+        
         
 
     private:
