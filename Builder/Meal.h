6bc6aec006b40dc65d98a4686243c56b0e8d5547
Katranas <148208752+Katranas@users.noreply.github.com>
2023-11-03 15:09:05 +0200

    Update Meal.h

--- Builder/Meal.h
+++ Builder/Meal.h
@@ -5,8 +5,10 @@ using namespace std;
 #include "Toppings.h"
 #include <string>
 #include <vector>
+#include <iostream>
 /**
- * @brief The PRodct of the builder Pattern
+ * @class Meal
+ * @brief The PRodct of the builder Pattern and the meal the customer will be served
  */
 class Meal {
     public:
@@ -15,13 +17,17 @@ class Meal {
         * @return A string describing the menu item.
         */
         Meal();
+        void setType(string);
         void setBase(string);
         void setSauce(string);
         void setCheese(bool);
         void addToppings(string);
+
+        void outputContents();
         
         
     private:
+        string type;
         string base;
         string sauce;
         bool cheese;
